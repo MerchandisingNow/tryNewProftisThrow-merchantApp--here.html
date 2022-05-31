9f7918d25a4fa97a2d606f59b1c6b2def7240622
MerchandisingNow <106112300+MerchandisingNow@users.noreply.github.com>
2022-05-31 12:20:40 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -3,7 +3,7 @@
 //localStorage.setItem("panier",pan);
 //localStorage.setItem("prod_panier","");
 //localStorage.setItem("total_p","0");
-localStorage.setItem("tl_prix","1000");
+//localStorage.setItem("tl_prix","1000");
 //localStorage.setItem("telechargements", "0");
 
 //affichage_produit.html.querySelector('article_for_image').innerHTML = ;
